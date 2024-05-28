3d443a38eaef2a383bb10e44da38a7e171d9cde3
novanota <notawhywhy@gmail.com>
2024-05-28 20:08:49 +0300

    added swiper and contact section

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,3 @@
 import mobileNav from './modules/mobile-nav.js';
+import { swiper } from './modules/swiper.js';
 mobileNav();
---
+++ src/js/modules/swiper.js
@@ -0,0 +1,7 @@
+export const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    // loop: true,
+    slidesPerView: 2.5,
+    spaceBetween: 14,
+    grabCursor: true,
+});
