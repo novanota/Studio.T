b7a97adba94b8328f210c7e4e354beac7ab5ec0b
novanota <notawhywhy@gmail.com>
2024-06-23 20:52:33 +0300

    fixed mobile menu

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -3,12 +3,22 @@ function mobileNav() {
     const navBtn = document.querySelector('.mobile-nav-btn');
     const nav = document.querySelector('.mobile-nav');
     const menuIcon = document.querySelector('.nav-icon');
+    const navLinks = document.querySelectorAll('.mobile-nav a');
 
     navBtn.onclick = function () {
         nav.classList.toggle('mobile-nav--open');
         menuIcon.classList.toggle('nav-icon--active');
         document.body.classList.toggle('no-scroll');
     };
+
+    // Close the mobile nav when a link is clicked
+    navLinks.forEach(link => {
+        link.onclick = function () {
+            nav.classList.remove('mobile-nav--open');
+            menuIcon.classList.remove('nav-icon--active');
+            document.body.classList.remove('no-scroll');
+        };
+    });
 }
 
 export default mobileNav;
