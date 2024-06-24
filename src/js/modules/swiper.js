9d27e4b8842cc292cd6ba827836923cc2c4e4ce9
novanota <notawhywhy@gmail.com>
2024-06-24 17:27:43 +0300

    swiper images fix

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -11,8 +11,24 @@ export const swiper = new Swiper(".swiper", {
             slidesPerView: 2.2,
         },
 
-        1295: {
+        1290: {
             slidesPerView: 2,
+        },
+
+        876: {
+            slidesPerView: 1.8,
+        },
+
+        676: {
+            slidesPerView: 1.5,
+        },
+
+        476: {
+            slidesPerView: 1.2,
+        },
+
+        376: {
+            slidesPerView: 1.1,
         },
     }
 });
